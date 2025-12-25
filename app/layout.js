c8449ff82e0f31b1a52c5c1cc043fc19a3278e4e
GaZeRnG <gazeringg@gmail.com>
2025-12-25 20:47:38 +0800

    reverted stuff

--- app/layout.js
+++
@@ -1,9 +0,0 @@
-// app/layout.js
-export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-        <head />
-        <body>{children}</body>
-        </html>
-    );
-}
--- app/page.js
+++
@@ -1,4 +0,0 @@
-// app/page.js
-export default function Home() {
-    return <h1>Beyond Wiki - coming soon</h1>;
-}
