265377e5fb375c040c1edc581c55016b74434480
GaZeRnG <gazeringg@gmail.com>
2026-02-24 01:25:07 +0800

    changed stuffs

--- beyond-depth/index.php
+++ beyond-depth/index.php
@@ -87,7 +87,7 @@
                 <p class="sector-title"><b>Bosses</b></p>
                 <!-- Mini -->
                 <div class="mini">
-                    <button onclick="dropfunction()" class="drop"><b>Mini-Bosses</b></button>
+                    <button onclick="dropfunction()" class="drop"><b>Mini-Bosses <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down-icon lucide-chevron-down"><path d="m6 9 6 6 6-6"/></svg></b></button>
                     <ul id="dropped" class="drop-content">
                         <li><a href="https://the-undergarden-mod.fandom.com/wiki/Forgotten_Guardian" target="_blank">Forgotten Guardian*</a></li>
                         <li><a href="https://github.com/ObscuriaLithium/Aquamirae/wiki/Creatures" target="_blank">Eel*</a></li>
--- beyond-depth/script.js
+++ beyond-depth/script.js
@@ -1,7 +1,16 @@
-document.addEventListener('DOMContentLoaded', dropfunction);
+import anime from 'animejs';
 
+document.addEventListener('DOMContentLoaded', dropfunction);
 function dropfunction() {
     document.querySelectorAll(".drop").forEach(b => 
     b.onclick = () => b.nextElementSibling.classList.toggle("active")
     );
 }
+
+const animation = anime({
+    targets: '.lucide-chevron-down',
+    rotate: '1turn',
+    duration: 200,
+    easing: 'easeInOutSine',
+    loop: true
+});
