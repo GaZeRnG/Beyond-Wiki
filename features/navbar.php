63c17689f58c8bb3d9c3ca5c05d496911d32ed50
GaZeRnG <gazeringg@gmail.com>
2026-03-01 16:59:38 +0800

    changed ui for navbar, register, and others

--- features/navbar.php
+++ features/navbar.php
@@ -5,21 +5,22 @@
 <section class="nav">
     <!-- Logo -->
     <?php if (isset($page) && $page !== 'hub'): ?>
-        <a href="/" class="logo_click">
+        <a href="/" class="nav_logo">
             <img src="/Images/Logo/Beyond_Wiki_logo_crop.webp" alt="Beyond Wiki Logo">
         </a>
     <?php else: ?>
         <img src="/Images/Logo/Beyond_Wiki_logo_crop.webp" alt="Beyond Wiki Logo">
     <?php endif; ?>
 
-    <section class="icons">
-        <div class="user" id="user">
+    <section class="drop">
+        <div class="user" tabindex="0" role="button">
             <!-- If logged in -->
             <?php if (isset($_SESSION["user_name"])): ?>
                 <p><?= htmlspecialchars($_SESSION["user_name"]) ?></p>
                 <div class="user-icon" id="user-icon">
                     <?php if (!empty($_SESSION["user_pfp"])): ?>
-                        <img src="<?= htmlspecialchars($_SESSION["user_pfp"]) ?>" alt="User Profile Picture" class="rounded-full w-6 h-6 object-cover">
+                        <img src="<?= htmlspecialchars($_SESSION["user_pfp"]) ?>" alt="User Profile Picture" class=" avatar rounded-full">
+                    <!-- If logged in but no pfp -->
                     <?php else: ?>
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-icon lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
                     <?php endif; ?>
@@ -33,15 +34,13 @@
                 </div>
             <?php endif; ?>
         </div>
-        <div class="dropdown" id="dropdown">
+        <ul tabindex="-1" class="drop_menu dropdown-content">
             <?php if (isset($_SESSION["user_name"])): ?>
-                <a href="/features/profile">Profile</a>
-                <a href="/features/logout/logout.php">Logout</a>
+                <li><a href="/features/profile">Profile</a></li>
+                <li><a href="/features/logout/logout.php">Logout</a></li>
             <?php else: ?>
-                <a href="/features/login">Login</a>
+                <li><a href="/features/login">Login</a></li>
             <?php endif; ?>
-        </div>
+        </ul>
     </section>
 </section>
-
-<script src="/features/navbar.js"></script>
--- features/register/index.php
+++ features/register/index.php
@@ -47,7 +47,6 @@
     </head>
     
     <body class="register-page">
-        <div id="bg" class="fixed inset-0 -z-10 bg-cover bg-center transition-all duration-1000 opacity-100 bg-1"></div>
         <!-- Navbar -->
         <?php require_once $_SERVER["DOCUMENT_ROOT"] . '/features/navbar.php'; ?>
 
@@ -69,22 +68,36 @@
             
             <!-- Form -->
             <form method="POST" class="register-form" enctype="multipart/form-data">
-                <label for="username">Username:</label>
-                <input type="text" id="username" name="username" required>
+                <!-- Username -->
+                <label for="username" class="input validator bg-neutral-800 rounded-md p-2.5 w-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-icon lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
+                    <input type="text" id="username" name="username" required placeholder="Username" pattern="[A-Za-z][A-Za-z0-9\-]*" minlength="3" maxlength="30" title="Only letters, numbers or dash"/>
+                </label>
+                <p class="validator-hint hidden mt-0">Must be 3 to 30 characters<br>Containing only letters, numbers or dash</p>
 
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email" required>
+                <!-- Email -->
+                <label for="email" class="input validator bg-neutral-800 rounded-md p-2.5 w-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mail-icon lucide-mail"><path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7"/><rect x="2" y="4" width="20" height="16" rx="2"/></svg>
+                    <input type="email" id="email" name="email" required placeholder="Email" title="Must be a valid email address"/>
+                </label>
+                <p class="validator-hint hidden mt-0">Enter valid email address</p>
+                
+                <!-- Password -->
+                <label for="password" class="input validator bg-neutral-800 rounded-md p-2.5 w-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-key-round-icon lucide-key-round"><path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"/><circle cx="16.5" cy="7.5" r=".5" fill="currentColor"/></svg>
+                    <input type="password" id="password" name="password" required placeholder="Password" minlength="8" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"/>
+                </label>
+                <p class="validator-hint hidden mt-0">Must be more than 8 characters, including number, lowercase letter, uppercase letter</p>
 
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" minlength="8" required>
-
-                <label for="confirm-password">Confirm Password:</label>
-                <input type="password" id="confirm-password" name="confirm-password" required>
+                <label for="confirm-password" class="input validator bg-neutral-800 rounded-md p-2.5 w-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-key-round-icon lucide-key-round"><path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"/><circle cx="16.5" cy="7.5" r=".5" fill="currentColor"/></svg>
+                    <input type="password" id="confirm-password" name="confirm-password" required placeholder="Confirm Password" minlength="8" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"/>
+                </label>
+                <p class="validator-hint hidden mt-0">Must be more than 8 characters, including number, lowercase letter, uppercase letter</p>
 
                 <button type="submit" class="register-submit">Register</button>
                 <a href="/features/login" class="register-back">Back to Login</a>
             </form>
         </div>
-        <script src="/features/register/register.js"></script>
     </body>
 </html>
