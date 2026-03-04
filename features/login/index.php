b6629f6d3a9cbbf0934c3b05a8d753945cdcfa98
GaZeRnG <gazeringg@gmail.com>
2026-03-05 02:36:14 +0800

    changed/updated register and login page ui

--- features/login/index.php
+++ features/login/index.php
@@ -54,51 +54,64 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale= 1.0">
     </head>
-    <body class="login-page">
-        <div id="bg" class="fixed inset-0 -z-10 bg-cover bg-center transition-all duration-1000 opacity-100 bg-1"></div>
+    <body class="login-page pb-0">
         <!-- Navbar -->
         <?php require_once $_SERVER["DOCUMENT_ROOT"] . '/features/navbar.php'; ?>
 
-        <div class="h-20"></div>
+        <section class="login-whole">
+            <div class="login-container ">
+                <section class="login-title">LOGIN</section>
 
-        <div class="login-container">
-            <section class="login-title">LOGIN</section>
+                <!-- For Errors -->
+                <?php if (!empty($errors)): ?>
+                    <div class="text-red-500 text-center mb-3">
+                        <?php foreach ($errors as $e): ?>
+                            <p><?= htmlspecialchars($e) ?></p>
+                        <?php endforeach; ?>
+                    </div>
+                <?php endif; ?>
 
-            <!-- For Errors -->
-            <?php if (!empty($errors)): ?>
-                <div class="text-red-500 text-center mb-3">
-                    <?php foreach ($errors as $e): ?>
-                        <p><?= htmlspecialchars($e) ?></p>
-                    <?php endforeach; ?>
-                </div>
-            <?php endif; ?>
+                <!-- Form -->
+                <form method="POST" class="login-form">
+                    <label for="username" class="input validator bg-neutral-800 rounded-md p-2.5 w-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-icon lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
+                        <input type="text" id="username" name="username" required placeholder="Username or Email" pattern="[A-Za-z][A-Za-z0-9\-]*" minlength="3" maxlength="30" title="Only letters, numbers or dash"/>
+                    </label>
+                    <p class="validator-hint hidden mt-0">Must be 3 to 30 characters<br>Containing only letters, numbers or dash</p>
 
-            <!-- Form -->
-            <form method="POST" class="login-form">
-                <label for="username">Username/Email:</label>
-                <input type="text" id="username" name="username" required>
+                    <label for="password" class="input validator bg-neutral-800 rounded-md p-2.5 w-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-key-round-icon lucide-key-round"><path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"/><circle cx="16.5" cy="7.5" r=".5" fill="currentColor"/></svg>
+                        <input type="password" id="password" name="password" required placeholder="Password" minlength="8" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"/>
+                    </label>
+                    <p class="validator-hint hidden mt-0">Must be more than 8 characters, including number, lowercase letter, uppercase letter</p>
 
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" required>
+                    <label class="login-remember">
+                        <input type="checkbox" id="remember-link" name="remember" class="checkbox bg-neutral-800 checked:bg-green-500" value="1">
+                        Remember Me
+                    </label>
 
-                <label class="login-remember">
-                    <input type="checkbox" id="remember-link" name="remember" class="login-remember-box" value="1">
-                    Remember Me
-                </label>
+                    <button type="submit" class="login-submit">Login</button>
+                    
+                    <div class="login-others">
+                        <!-- Google -->
+                        <button class="login-google">
+                            <a href="/features/oauth/google.php" id="google-link" name="google">
+                                <img class="w-5 h-5" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo">
+                                Google</a>
+                        </button>
 
-                <button type="submit" class="login-submit">Login</button>
-                
-                <div class="login-others">
-                    <!-- Google -->
-                    <a href="/features/oauth/google.php" id="google-link" name="google" class="login-google">Google</a>
+                        <!-- Discord -->
+                        <button class="login-discord">
+                            <a href="/features/oauth/discord.php" id="discord-link" name="discord">
+                                <img class="w-5 h-5" src="https://www.svgrepo.com/show/353655/discord-icon.svg" loading="lazy" alt="discord logo">
+                                Discord</a>
+                        </button>
+                    </div>
 
-                    <!-- Discord -->
-                    <a href="/features/oauth/discord.php" id="discord-link" name="discord" class="login-discord">Discord</a>
-                </div>
-
-                <a href="/features/register" class="login-register">Don't have an account? Register here.</a>
-            </form>
-        </div>
+                    <a href="/features/register" class="login-register">Don't have an account? Register here</a>
+                </form>
+            </div>
+        </section>
         <script src="/features/login/login.js"></script>
     </body>
 </html>
